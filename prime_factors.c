88ae8d5e3ebd63c4a73283655310d2aca37d9dc3
Harish-kumar1 <harishkumarss05@gmail.com>
2021-02-09 17:25:09 +0530

    added core logic of function

--- prime_factors.c
+++ prime_factors.c
@@ -24,5 +24,35 @@ int main()
 /*Function definition*/
 void prime_fac(int x)
 {
+ /*Loop to find one part of the prime factors in case the number is even*/
+ while(x % 2 == 0)
+ {
+  x = x / 2;
+  printf("2\n");
+ }
  
+ /*Loop to find one part the prime factors in case the number is divisible by 3*/
+ while(x % 3 == 0)
+ {
+  x = x / 3;
+  printf("3\n");
+ }
+ 
+ /*Loop to find one part of the prime factors in case the number is divisible by 5*/
+ while(x % 5 == 0)
+ {
+  x = x / 5;
+  printf("5\n");
+ }
+ 
+ /*Loop to find one part of the prime factors in case the number is divisible by 7*/
+ while(x % 7 == 0)
+ {
+  x = x / 7;
+  printf("7\n");
+ }
+ 
+ /*Condition to check if x has been decomposed to 1 and print it if not the case*/
+ if(x != 1)
+  printf("%d\n", x);
 }
