bf21ea5e58c5e50daf2ea1260387cdf9cb041009
Harish-kumar1 <harishkumarss05@gmail.com>
2021-02-09 20:35:41 +0530

    Replacing previous logic with a recursion

--- prime_factors.c
+++ prime_factors.c
@@ -9,50 +9,32 @@ void prime_fac(int);
 int main()
 {
  /*Type declaration*/
- int n;
+ int num;
  
  /*Obtaining input from the user*/
  printf("Please enter a positive integer:\n");
- scanf("%d", &n);
+ scanf("%d", &num);
  
  /*Function call*/
- prime_fac(n);
+ prime_fac(num);
  
  return 0;
 }
 
 /*Function definition*/
-void prime_fac(int x)
+void prime_fac(int num)
 {
- /*Loop to find one part of the prime factors in case the number is even*/
- while(x % 2 == 0)
- {
-  x = x / 2;
-  printf("2\n");
- }
+ /*Type declaration*/
+ int x;
  
- /*Loop to find one part the prime factors in case the number is divisible by 3*/
- while(x % 3 == 0)
+ /*Loop to run through numbers from 2 to check for exactly divisible number*/
+ for(x = 2; x <= num; x++)
  {
-  x = x / 3;
-  printf("3\n");
+  if(num % x == 0)
+  {
+   printf("%d\n", x);
+   prime_fac(num / x);
+   break;
+  }
  }
- 
- /*Loop to find one part of the prime factors in case the number is divisible by 5*/
- while(x % 5 == 0)
- {
-  x = x / 5;
-  printf("5\n");
- }
- 
- /*Loop to find one part of the prime factors in case the number is divisible by 7*/
- while(x % 7 == 0)
- {
-  x = x / 7;
-  printf("7\n");
- }
- 
- /*Condition to check if x has been decomposed to 1 and print it if not the case*/
- if(x != 1)
-  printf("%d\n", x);
 }
