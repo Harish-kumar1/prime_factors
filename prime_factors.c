6e1a639f3097522bef5f4a7bddbeace36c69a0f7
Harish-kumar1 <harishkumarss05@gmail.com>
2021-02-09 16:08:46 +0530

    first commit

---
+++ prime_factors.c
@@ -0,0 +1,28 @@
+/*The following program determines the prime factors of a positive integer entered by the user using a general purpose function*/
+/*Author: Harish Kumar Date: 09-02-2021*/
+
+# include <stdio.h>
+
+/*Function prototype declaration*/
+void prime_fac(int);
+
+int main()
+{
+ /*Type declaration*/
+ int n;
+ 
+ /*Obtaining input from the user*/
+ printf("Please enter a positive integer:\n");
+ scanf("%d", &n);
+ 
+ /*Function call*/
+ prime_fac(n);
+ 
+ return 0;
+}
+
+/*Function definition*/
+void prime_fac(int x)
+{
+ 
+}
